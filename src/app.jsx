a5acb1cf664748d830528e5fb50e0faf9b7fd792
Sviatoslav Sobakin <slavasobb@gmail.com>
2020-02-12 00:06:19 +0300

    init commit

---
+++ src/app.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+// eslint-disable-next-line arrow-body-style
+export const App = () => {
+  return (
+    <div className="pt-2">
+      <h2>Welcome to React kek!</h2>
+    </div>
+  )
+}
